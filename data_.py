bd0db21c40f425ae5f60d5c2ad03d6a2ac530dab
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-01-09 11:56:03 +0300

    docstring and formate the code

--- data_.py
+++ data_.py
@@ -1,24 +1,32 @@
 import pandas as pd
+import os
 
 from driver_config import FlaurologicalDriver
 
 
 class FlaurologicalDataStructure:
 
-    def __init__(self, url):
+    def __init__(self, url: str):
+        self.data = []
+        self.df = None
+
         driver = FlaurologicalDriver()
         driver.run(url)
         driver.quit()
 
-        self.data = []
-        self.df = None
-
         self.preprocess_data(driver.data)
         self.save()
 
-    def preprocess_data(self, old_data):
+    def preprocess_data(self, old_data: list) -> None:
+        """
+        Preprocess fields of the data in list of dicts format.
+         - names to First Name, Middle Name, Last Name
+         - session-title to the Session Title and Title field
+         - speakerType to Position
+        :param old_data:
+        :return:
+        """
         session_title = ''
-        title = ''
         for d in old_data:
             if 'session' in d['session-title'].lower():
                 title = ''
@@ -28,20 +36,22 @@ class FlaurologicalDataStructure:
             for i in range(len(d['speakerDetails']['names'])):
                 self.data.append(self._gen_data(d, speaker_index=i, session_title=session_title, title=title))
         self._data_to_dataframe()
-        print(self.data)
 
-    def save(self):
-        self.df.to_csv('data/flaurologicaldataset.csv')
+    def save(self) -> None:
+        """
+        Save the dataframe `self.df` into the '.csv' file
+        :return: None
+        """
+        self.df.to_csv(os.path.join('data', 'flaurologicaldataset.csv'))
 
-    def _gen_data(self, d, *args, **kwargs):
+    def _gen_data(self, d: dict, *args, **kwargs) -> dict:
         new_data = {}
         new_data['Session Title'] = kwargs['session_title']
         new_data['Title'] = kwargs['title']
 
         new_data['Position'] = d['speakerType'].strip(':').strip('s')  # Example: 'Speakers:'
         name = d['speakerDetails']['names'][kwargs['speaker_index']]
-        name = name[:name.find(',')]
-        name = name.split(' ')
+        name = name[:name.find(',')].split(' ')
         new_data['First Name'] = name[0]
         if len(name) > 2:
             new_data['Middle Name'] = name[1].strip('.')
--- driver_config.py
+++ driver_config.py
@@ -12,6 +12,10 @@ class FlaurologicalDriver:
     DIR_PATH = 'pages'
 
     def __init__(self, logger=None):
+        self.logger = logger
+        self.pages = []
+        self.data = []
+
         try:
             display = Display(visible=0, size=(800, 600))
             display.start()
@@ -25,33 +29,39 @@ class FlaurologicalDriver:
                 logger.error(_er)
             self.driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 
-        self.pages = []
-        self.data = []
-
-    def run(self, url):
+    def run(self, url: str, save_mode=False) -> None:
+        """
+        Starting point parser
+        :param url: <str> web-url to scrape
+        :param save_mode: <bool> default=False. If true then it will run the save_pages method
+        :return: None
+        """
+        if save_mode:
+            self.save_pages(url)
         self.parser(url)
-        # self.save_pages(url)
 
-    def parser(self, url):
+    def parser(self, url: str) -> None:
+        """
+        Parse whole the data from the url
+        :param url: <str> web-url to scrape
+        :return: None
+        """
         self.driver.get(url)
-        # self._specific_first_page()  # not really need
-        # counter = 1
         while True:
             btn = self.driver.find_element_by_xpath('//*[@id="FUS1808_next"]')
-            # counter += 1
             if btn.get_attribute("class").split(' ')[-1] == 'next':
                 self._specific_page()
                 btn.click()
             else:
                 self._specific_page()
                 break
-            # if counter == 3:
-            #     print(self.data)
-            #     exit()
-        # self._specific_last_page()  # not really need
-        # print(self.data)
 
-    def save_pages(self, url):
+    def save_pages(self, url: str) -> None:
+        """
+        Save every page from the web-url
+        :param url: <str> web-url to scrape
+        :return: None
+        """
         self.driver.get(url)
         counter = 0
         while True:
@@ -65,36 +75,47 @@ class FlaurologicalDriver:
                 self.__download_page(counter)
                 break
 
-    def quit(self):
+    def quit(self) -> None:
+        """
+        Stop the driver. Quit the browser
+        :return: None
+        """
         self.driver.quit()
 
     # PROTECTED
-    def _specific_first_page(self):
+    def _specific_first_page(self) -> None:
+        """
+        Parse only first page, which contains only 1 required row
+        :return: None
+        """
         data = {}
         data['session-title'] = self.__xpath_text('//*[@id="FUS1808"]/tbody/tr[10]/td[3]/div[1]')
         data['speakerType'] = self.__xpath_text('//*[@id="FUS1808"]/tbody/tr[10]/td[3]/div[2]/div[1]')
         data['speakerDetails'] = {}
         elem = self.__xpath_text('//*[@id="FUS1808"]/tbody/tr[10]/td[3]/div[2]/div[2]')
         data['speakerDetails']['name'], data['speakerDetails']['Workplace'] = elem.split('\n')
-        # print(data)
         self.data.append(data)
 
-    def _specific_page(self):
+    def _specific_page(self) -> None:
+        """
+        Parse every page
+        :return: None
+        """
         table = self.driver.find_element_by_xpath('//*[@id="FUS1808"]/tbody')
         elements = table.find_elements(By.TAG_NAME, 'tr')
         for element in elements:
             try:
                 self.data.append(self._gen_data(element))
             except Exception as _er:
-                # print(_er)
-                pass
+                if self.logger:
+                    self.logger.error(_er)
 
-    def _specific_last_page(self):
-        pass
-
-    def _gen_data(self, element):
+    def _gen_data(self, element) -> dict:
+        """
+        Gen data from the page to dict format
+        :return: None
+        """
         data = {}
-        # data['session-title'] =
         data['session-title'] = element.find_elements(By.TAG_NAME, 'td')[2].find_elements(By.TAG_NAME, 'div')[0].text
         data['speakerType'] = element.find_elements(
             By.TAG_NAME, 'td'
@@ -109,7 +130,6 @@ class FlaurologicalDriver:
             workplaces = []
             elem = list(filter(lambda x: x, elem))
             for i in range(len(elem)):
-                print(elem)
                 if i % 2 == 0:
                     names.append(elem[i])
                 else:
@@ -120,12 +140,26 @@ class FlaurologicalDriver:
         return data
 
     # PRIVATE
-    def __xpath_text(self, xpath, elem=None):
+    def __xpath_text(self, xpath: str, elem=None) -> str:
+        """
+        Get the text from the `self.driver` element or `elem` if was received.
+        Find the element via `xpath`
+        :param xpath: <str>
+        :param elem: selenium-object
+            if elem is not None:
+                return elem.find_element_by_xpath(xpath).text
+        :return: <str> text of the selenium-element
+        """
         if elem:
             return elem.find_element_by_xpath(xpath).text
         return self.driver.find_element_by_xpath(xpath).text
 
-    def __download_page(self, i):
+    def __download_page(self, i: int) -> None:
+        """
+        load page from the url
+        :param i: <int> number of the page. It will be the specific name of '.html' document
+        :return: None
+        """
         page = self.driver.page_source
         filepath = os.path.join(self.DIR_PATH, str(i) + '.html')
         with open(filepath, 'w') as f:
